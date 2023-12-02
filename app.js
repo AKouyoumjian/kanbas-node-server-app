cfe67e6fed6fcc02b9db9a94d78d4a4f98b9d797
Alexander Kouyoumjian <123589862+AKouyoumjian@users.noreply.github.com>
2023-12-02 16:58:41 -0500

    cors

--- app.js
+++ app.js
@@ -20,8 +20,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    // origin: process.env.FRONTEND_URL,
-    origin: "*",
+    origin: process.env.FRONTEND_URL,
   })
 );
 const sessionOptions = {
